50e1654209ae362fce384d4b12ff28866dcb3f84
Diana Shafa A. <158548646+shazidian@users.noreply.github.com>
2026-02-28 10:23:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function getSectionContent(sectionId) {
                 Untuk menjaga keamanan dan kesehatan WBP, terdapat beberapa jenis makanan yang tidak diperbolehkan dibawa saat berkunjung. Silakan perhatikan poster informasi berikut untuk mengetahui detail lengkap makanan yang dilarang.
             </p>
             <div class="poster-area">
-                <div class="poster-placeholder">
+                <div class="poster.png">
                     <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                     </svg>
@@ -86,7 +86,7 @@ function getSectionContent(sectionId) {
                 Pengunjung wajib mengenakan pakaian yang sopan dan sesuai dengan ketentuan yang berlaku. Lihat poster informasi di bawah ini untuk mengetahui aturan berpakaian lengkap saat berkunjung ke Lapas.
             </p>
             <div class="poster-area">
-                <div class="poster-placeholder">
+                <div class="poster.png">
                     <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                     </svg>
